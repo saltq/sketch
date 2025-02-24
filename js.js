32747640cf9fab9395e2de9f53b8b835468512c1
saltq <59333310+saltq@users.noreply.github.com>
2025-02-24 21:29:35 +0500

    add style flares, input buttons and functioning grid

--- js.js
+++ js.js
@@ -1,19 +1,19 @@
 const container = document.getElementById("enclosure");
 function createCell(x,q){
     const cell = document.createElement("div");
-    cell.style.backgroundColor = "cyan";
+    cell.style.backgroundColor = "black";
     cell.textContent = q;
     cell.style.alignContent ="center";
     cell.style.textAlign="center";
     cell.style.maxHeight = "calc(100%/" +(x)+")"
     cell.style.maxWidth = "calc(100%/" +(x)+")"
-    cell.style.flexBasis = "calc(100%/" +(x)+")"; // the grid needs to be divided by x+1 parts
-    //cell.style.flexBasis = "calc(100%/4 - 10px)";
+    cell.style.flexBasis = "calc(100%/" +(x)+")"; 
+    cell.addEventListener("mouseenter", (e) =>{
+        console.log(e.target.id);
+        cell.style.backgroundColor = "white";
+    })
     container.appendChild(cell);
-}
-function wrap(){
-    const nl = document.createElement("pre");
-    container.appendChild(nl);
+    
 }
 function generateGrid(x){
     for (q = 0; q<x; q++){
@@ -23,4 +23,33 @@ function generateGrid(x){
     
     }
 }
-generateGrid(16);
+function deleteGrid(x){
+    for (q = 0; q<x; q++){
+        for (i = 0; i<x; i++){
+            container.removeChild(cell)
+        }
+    
+    }
+}
+let curGridVal = 0;
+
+const input = document.getElementById("item");  
+const buttons = document.getElementById("buttons");
+buttons.addEventListener("click", (e) => {
+    console.log(e.target.id);
+    switch (e.target.id){
+        case "+":
+            deleteGrid(curGridVal);
+            generateGrid(curGridVal++);
+            break;
+        case "-":
+            generateGrid(curGridVal--);
+            break;
+        case "enterBtn":
+            curGridVal = input.value
+            generateGrid(curGridVal);
+            break;
+        default:
+            break;
+    }
+})
